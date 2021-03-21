1d85bc99e3ed6a6b3d768c8b91f22ee41764fafa
Neil <zooming.in@hotmail.com>
2021-03-21 22:05:05 +1100

    Draft assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const name = ["Lisa", "Kaitlin", "Jan"];
+function writeCards (name) {
+  for (let i = 0; i < name.length; i++) {
+    console.log(`Thank you, ${name[i]}, for the wonderful birthday gift!`);
+    debugger;
+  }
+  return name;
+}
+writeCards(name);
+
+
+let countDown = 10;
+while (countDown > 0) {
+    console.log(countDown--);
+}
+
+countDown;
